c556e9fc4aa59657ac4d9ae798dd4e14ceb1465c
David Marin <ddmarin94@gmail.com>
2017-08-03 20:02:04 -0700

    SearchBar capturing input value & show it in the console

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './SearchBar.css'
 
-const SearchBar = (props) => (
-  <div className="SearchBarPosition">
-    <input placeholder="Username" />
-    <button>Search</button>
-  </div>
-)
-
-
+class SearchBar extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {value: ''}
+    //We don't need the code below because we are using the bing that arrow function includes! :D
+    //this.handleChange = this.handleChange.bind(this)
+    //this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleChange = (event) => {
+    this.setState({value: event.target.value})
+  }
+  handleSubmit = (event) => {
+    console.log(this.state.value)
+    event.preventDefault()
+  }
+  render () {
+    return (
+      <div className='SearchBarPosition'>
+        <form onSubmit={this.handleSubmit}>
+          <input placeholder='Username' type='text' value={this.state.value} onChange={this.handleChange} />
+          <input type='submit' value='Submit' placeholder='Search' />
+        </form>
+      </div>
+    )
+  }
+}
 export default SearchBar
